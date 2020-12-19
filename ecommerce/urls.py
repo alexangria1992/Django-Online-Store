f25b668543501bba98aa8908e4d6a1c490a64461
alexangria1992 <alex040892@gmail.com>
2020-12-20 11:57:33 +1300

    Product Image gallery

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,6 @@ urlpatterns = [
     path('home/', include('home.urls')),
     path('product/', include('product.urls'))
 
-]
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
