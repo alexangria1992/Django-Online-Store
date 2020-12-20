a962adb76e96926044fee028f38363a20e5d77a1
alexangria1992 <alex040892@gmail.com>
2020-12-20 22:18:33 +1300

    Contact Page

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from home import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
+    path('about/', views.aboutus, name='aboutus'),
+    path('contact/', views.contactus, name='contactus'),
     path('home/', include('home.urls')),
     path('product/', include('product.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
--- home/views.py
+++ home/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from home.models import Setting
 # Create your views here.
 def index(request):
-    university="Karabuk University"
-    dept="Computer Engineering"
-    context={'university':university, 'department':dept}
+    setting = Setting.objects.get(pk=1)
+    page = "home"
+    context={'setting':setting, 'page':page}
     return render(request, 'index.html', context)
+
+def aboutus(request):
+    setting = Setting.objects.get(pk=1)
+    context={'setting':setting}
+    return render(request, 'about.html', context)
+
+def contactus(request):
+    setting = Setting.objects.get(pk=1)
+    context={'setting':setting}
+    return render(request, 'contactus.html', context)
+    
