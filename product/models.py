f36e61ff43749dd4950684f72fff8c42305ca80a
alexangria1992 <alex040892@gmail.com>
2020-12-23 20:42:57 +1300

    Creating Slug for Category

--- product/models.py
+++ product/models.py
@@ -3,6 +3,7 @@ from django.utils.safestring import mark_safe
 from ckeditor_uploader.fields import RichTextUploadingField
 from mptt.fields import TreeForeignKey
 from mptt.models import MPTTModel
+from django.urls import reverse
 
 # Create your models here.
 
@@ -17,7 +18,7 @@ class Category(MPTTModel):
     description = models.CharField(max_length=255)
     image = models.ImageField(blank = True, upload_to='images/')
     status = models.CharField(max_length=10, choices = STATUS)
-    slug = models.SlugField()
+    slug = models.SlugField(null = False, unique = True)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
@@ -26,6 +27,9 @@ class Category(MPTTModel):
 
     class MPTTMeta:
         order_insertion_by = ['title']
+
+    def get_absolute_url(self):
+        return reverse('category_detail', kwargs={'slug':self.slug})
 
     def __str__(self):
         full_path = [self.title]
@@ -42,14 +46,14 @@ class Product(models.Model):
     )
     category = models.ForeignKey(Category, on_delete=models.CASCADE) # many to one relation with Category
     title = models.CharField(max_length=150)
-    keywords = models.CharField(max_length=30)
+    keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank = True, upload_to='images/')
     price = models.FloatField()
     amount = models.IntegerField()
     minamount = models.IntegerField()
     detail = RichTextUploadingField()
-    slug = models.SlugField()
+    slug = models.SlugField(null = False, unique = True)
     status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
@@ -61,6 +65,9 @@ class Product(models.Model):
         return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
     
     image_tag.short_description = 'Image'
+
+    def get_absolute_url(self):
+      return reverse('category_detail', kwargs={'slug':self.slug})
 
 class Images(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
